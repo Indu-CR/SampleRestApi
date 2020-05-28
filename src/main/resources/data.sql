fc9c5b49d9175f5ff9047def444f59e6f6b120b4
Vinay Chilukuri <40259957+Vc91234@users.noreply.github.com>
2020-05-28 13:16:48 +0530

    Updated data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,4 @@ insert into robber values (2, 'Rio', 'Brazil');
 insert into robber values (3, 'Nairobi', 'Kenya');
 insert into robber values (4, 'Denver', 'Colorado');
 insert into robber values (5, 'Moscow', 'Russia');
+insert into robber values (5, 'Berlin', 'Germany');
